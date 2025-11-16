ef28ba77165957b5a6dcce432cf65bf31337b7a9
idozri <64650097+idozri@users.noreply.github.com>
2025-11-16 09:37:15 +0200

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,26 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
-import { componentTagger } from 'lovable-tagger';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: '::',
+    host: "::",
     port: 3000,
   },
-  plugins: [react(), mode === 'development' && componentTagger()].filter(
-    Boolean
-  ),
+  plugins: [
+    react(),
+    ...(mode === "development"
+      ? [
+          componentTagger({
+            allowedHosts: ["localhost", "mini-chatgpt-frontend.onrender.com"],
+          }),
+        ]
+      : []),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
-  },
-  preview: {
-    allowedHosts: ["*"],
   },
 }));
